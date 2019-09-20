4561a0f8438d046eed340b0e14d5160f345378a9
Jacqueline Lam <jacqueline.karin.lam@gmail.com>
2019-09-20 16:35:01 +0000

    Automatically backed up by Learn

--- lib/my_all.rb
+++ lib/my_all.rb
@@ -2,11 +2,12 @@ require 'pry'
 
 def my_all?(collection)
   i = 0 
+  block_return_values = []
+  
   while i < collection.length
     #send whatever passed in as the argument ot the block
-    yield(collection[i]) #yied each ele in collection to the block
+    block_return_values << yield(collection[i]) #yied each ele in collection to the block
     i += 1 
-  # binding.pry
   end
 end
 
